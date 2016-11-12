d845e78ea4292f96425e42bb86bc34c6600a0272
Ian Boucher <i.a.boucher@gmail.com>
2016-11-12 16:22:26 +0000

    Layout basic landing page

---
+++ public/js/nav/NavCtrl.js
@@ -0,0 +1,13 @@
+(function()
+{
+    "use strict";
+
+    angular
+        .module("learnary")
+        .controller("NavCtrl", ["$scope",
+            function NavCtrl($scope)
+            {
+                self = this;
+            }
+        ]);
+})();
--- public/views/index.php
+++ public/views/index.php
@@ -4,28 +4,33 @@
 <head>
     <title>Learnary</title>
 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="icon" href="../../favicon.ico">
 
     <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans:400,800,600,700,300">
     <link rel="stylesheet" type="text/css" href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- <link rel="stylesheet" type="text/css" href="/styles/normalize.css"> -->
     <link rel="stylesheet" type="text/css" href="/css/main.css">
+    <link rel="stylesheet" type="text/css" href="/css/landing.css">
 
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <!-- <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet"> -->
+
+    <!-- <script src="../../assets/js/ie-emulation-modes-warning.js"></script> -->
 
 </head>
 
 <body>
 
-    <div class="container">
-        <div class="row row-centered">
-            <div class="col-xs-10 col-centered">
 
-                <ui-view></ui-view>
+    <ui-view></ui-view>
 
-            </div>
-        </div>
-    </div>
 
     <!-- APPLICATION DEPENDENCIES -->
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
@@ -35,6 +40,7 @@
     <!-- APPLICATION SCRIPTS -->
     <script src="/js/app.js"></script>
     <script src="/js/landing/LandingCtrl.js"></script>
+    <script src="/js/landing/NavCtrl.js"></script>
 
 </body>
 </html>
