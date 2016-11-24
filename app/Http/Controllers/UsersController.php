f6615f8a7fcf7467d08a65353d743c4cf8da37e5
Ian Boucher <i.a.boucher@gmail.com>
2016-11-24 10:17:28 +0000

    Create basic user listing view

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -24,7 +24,7 @@ class UsersController extends Controller
 
     public function index()
     {
-        $users = User::all();
+        $users = User::with('roles')->get();
         return $users;
     }
 
--- public/js/user/UserCtrl.js
+++ public/js/user/UserCtrl.js
@@ -12,21 +12,18 @@
                 self.userData;
                 self.error;
 
-                self.getUsers = function()
-                {
-                    $http.get("api/v1.0.0/users").then(
-                        function(users)
-                        {
-                            self.userData = users.data;
-                        },
-                        function(errorData)
-                        {
-                            self.error = errorData.data.error;
-                            console.log(errorData); // TODO: handle error properly
-                            // window.alert("Unable to retrieve user data")
-                        }
-                    );
-                }
+                $http.get("api/v1.0.0/users").then(
+                    function(users)
+                    {
+                        self.userData = users.data;
+                    },
+                    function(error)
+                    {
+                        self.error = error.data.error;
+                        console.log(errorData); // TODO: handle error properly
+                        // window.alert("Unable to retrieve user data")
+                    }
+                );
             }
         ]);
 })();
